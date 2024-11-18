ffaae0e647a2874b8128c1e06f756d952c466baf
KateW <75755942+noirkei7955@users.noreply.github.com>
2024-11-18 12:08:58 +0800

    Update thank-you-page.php

--- Client/thank-you-page.php
+++ Client/thank-you-page.php
@@ -6,17 +6,23 @@ $shipping = 30;
 
 echo "<h1>Order Summary</h1>"." for ".$cname."<br/><br/>";
 echo "Item<br/>";
-foreach ($items as $item => $price){
-  echo $item."x".$times."$".$price;
+$subtotal = 0; 
+foreach ($items as $index => $item) {
+  $quantity = 1;
+  $price = $amounts[$index];
+  echo htmlspecialchars($item) . " x " . $quantity . " = $" . number_format($price * $quantity, 2) . "<br/>";
+  $subtotal += $price * $quantity;
 }
 
-echo "Subtotal<br/><br/><br/><br/><br/><br/>".;
-if (<100) {
-echo "Shipping<br/><br/><br/><br/><br/><br/>"."$".$shipping;
+echo "Subtotal<br/><br/><br/><br/><br/><br/>$". number_format($subtotal, 2);
+if ($subtotal<100) {
+echo "Shipping<br/><br/><br/><br/><br/><br/>"."$".number_format($shipping, 2)."<br/>";
+ $total = $subtotal + $shipping;
+  "<b>Total</b><br/>$" . "<b>".number_format($total, 2) . "</b>";
 }
-else{echo "Shipping<br/><br/><br/><br/><br/><br/>"."$".0;}
-
-echo "<b>Total<br/><br/><br/><br/><br/><br/></b>"."<b></b>";
+else{echo "Shipping<br/><br/><br/><br/><br/><br/>"."$".0;
+    $total = $subtotal;
+    echo"<b>Total</b><br/>$" . "<b>".number_format($total, 2) . "</b>"}
 
 echo "Thank you!";
 ?>
