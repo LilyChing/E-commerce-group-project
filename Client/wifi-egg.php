1d0b5f796a88f9b9c21daf27dac857b34506069c
dannyfung9567 <dannyfung9567@gmail.com>
2024-11-25 00:59:31 +0800

    add wifi egg
    danny

---
+++ Client/wifi-egg.php
@@ -0,0 +1,75 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Product Page</title>
+  <link rel="stylesheet" href="wifi-egg.css">
+</head>
+    <body>
+        <div class="product-container">
+            <div class="product-card">
+            <h1>WiFi  Egg / SIM Cards</h1>
+            </div> 
+        </div>
+        <div class="product-container">
+            <div class="product-card">
+                <img src="./asset/SIM_temp.png" class="custom-size">
+                    <div class="product-info">
+                        <div class="product-title">Product Name Title Here</div>
+                        <div class="product-price">$99</div>
+                        <div>Category</div>
+                        <p>Proin in ullamcorper lorem. Maecenas eu ipsum.</p>
+                        <button class="add-to-cart">Add to cart</button>
+                    </div>
+            </div>
+        </div>
+        <div class="product-container">
+            <div class="product-card">
+                <img src="./asset/SIM_temp.png" class="custom-size">
+                    <div class="product-info">
+                        <div class="product-title">Product Name Title Here</div>
+                        <div class="product-price">$999</div>
+                        <div>Category</div>
+                        <p>Proin in ullamcorper lorem. Maecenas eu ipsum.</p>
+                        <button class="add-to-cart">Add to cart</button>
+                    </div>
+            </div>
+        </div>
+        <div class="product-container">
+            <div class="product-card">
+                <img src="./asset/SIM_temp.png" class="custom-size">
+                    <div class="product-info">
+                        <div class="product-title">Product Name Title Here</div>
+                        <div class="product-price">$989</div>
+                        <div>Category</div>
+                        <p>Proin in ullamcorper lorem. Maecenas eu ipsum.</p>
+                        <button class="bi bi-cart-fill">Add to cart</button>
+                    </div>
+            </div>
+        </div>
+        <div class="product-container">
+            <div class="product-card">
+                <img src="./asset/SIM_temp.png" class="custom-size">
+                    <div class="product-info">
+                        <div class="product-title">Product Name Title Here</div>
+                        <div class="product-price">$99</div>
+                        <div>Category</div>
+                        <p>Proin in ullamcorper lorem. Maecenas eu ipsum.</p>
+                        <button class="add-to-cart">Add to cart</button>
+                    </div>
+            </div>
+        </div>
+        <div class="product-container">
+            <div class="product-card">
+                <img src="./asset/SIM_temp.png" class="custom-size">
+                    <div class="product-info">
+                        <div class="product-title">Product Name Title Here</div>
+                        <div class="product-price">$99</div>
+                        <div>Category</div>
+                        <p>Proin in ullamcorper lorem. Maecenas eu ipsum.</p>
+                        <button class="add-to-cart">Add to cart</button>
+                    </div>
+            </div>
+        </div>
+    </body>
+</html>
