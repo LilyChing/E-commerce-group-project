287917ad0ae0b48c0b7187f9f81637746029442b
LilyChing <106129893+LilyChing@users.noreply.github.com>
2024-11-25 23:43:25 +0800

    Add page title & page icon

--- Client/index.php
+++ Client/index.php
@@ -14,6 +14,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Change the title to your page name -->
+  <title>Home | Leading Duck Tourism</title>
+  <link rel="icon" href="./asset/rubber-duck-yellow.png" type="image/icon type">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
